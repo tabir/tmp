c950881a0642cb8119666dbe9ed1419cffca491d
pudera <avi.puder@emc.com>
2019-04-10 19:09:10 +0300

    update3
    Signed-off-by: pudera <avi.puder@emc.com>

--- scripts/merge_csv.py
+++ scripts/merge_csv.py
@@ -3,7 +3,6 @@
     usage: merge_csv.py {2+ csv-files} > out.csv
     Note:
         1. Will skip lines with dates that are not common to all inputs
-        2. will sum up data in common columns
 
 """
 
@@ -27,18 +26,6 @@ for filename in inputs:
 dates = {}
 dates_occ = {}
 
-def get_val(s):
-    """
-    convert string to numeric value:
-    '' --> 0
-    '1.3' --> 1.3
-    """
-    if s == '':
-       return 0
-    else:
-       return float(s)
-
-
 # read and output the merge data
 writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
 writer.writeheader()
@@ -46,7 +33,6 @@ for filename in inputs:
     with open(filename, "r") as f_in:
         reader = csv.DictReader(f_in)  # Uses the field names in this file
         for line in reader:
-            
             
             date = line['date']
             
@@ -58,13 +44,8 @@ for filename in inputs:
             # merge the data into "line"
             if date in dates:
                 prev_data = dates[date]
-                for key in prev_data:
-                    
-                    #print "DBG1: key:", key, " date:", date
+                for key in prev_data:                    
                     if key != 'date':
-                        if key in line:
-                            1 # line[key] = get_val(line[key]) + get_val(prev_data[key])
-                        else:
                             line[key] = prev_data[key]
             
             dates[date] = line
